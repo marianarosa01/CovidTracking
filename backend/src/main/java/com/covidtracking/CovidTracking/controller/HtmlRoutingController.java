4ab7e92ec2a196c7793b6069021d3239c041ee6e
Mariana Rosa <marianarosa@ua.pt>
2022-04-21 17:09:54 +0100

    html

--- backend/src/main/java/com/covidtracking/CovidTracking/controller/HtmlRoutingController.java
+++ backend/src/main/java/com/covidtracking/CovidTracking/controller/HtmlRoutingController.java
@@ -1,26 +1,22 @@
 package com.covidtracking.CovidTracking.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HtmlRoutingController {
-    @RequestMapping(value="/index")
+    @RequestMapping(value = "/index")
     public static String welcome() {
         return "index";
     }
 
-    @RequestMapping(value="/countrydata")
-    public static String countrysearch(){return "countrydata";}
-
-    @RequestMapping(value="/sixmonthsstatistics")
-    public static String airQualitySearch() {
-        return "airqualitysearch";
+    @RequestMapping(value = "/countrydata")
+    public static String countrysearch() {
+        return "countrydata";
     }
 
-    @RequestMapping(value="/allcountries")
-    public static String weatherSearch() {
-        return "weathersearch";
+    @RequestMapping(value = "/cache")
+    public static String cacheStat() {
+        return "cache";
     }
 }
